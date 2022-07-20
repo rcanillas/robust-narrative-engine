a3e461cf1072eaa647740a90f4b2621bbc0a6643
Rémi Canillas <remi.canillas@insa-lyon.fr>
2022-07-20 04:54:14 +0200

    premisse generator v1

--- playground.py
+++ playground.py
@@ -14,7 +14,8 @@ ch_reason = ["repair $pr1 past mistakes",
              "achieve $pr1 destiny",
              "avoid a prophecy",
              "avenge $pr1 family",
-             "be rich"]
+             "be rich",
+             "win the admiration of the one $pr0 loves"]
 incompatible_reasons_map = {"settle in peace": ["become the hero of common folks",
                                                 "be rich",
                                                 "avenge $pr1 family",
@@ -24,34 +25,49 @@ ch_sex = ["male", "female", "fluid"]
 pronouns_map = {"male": ("he", "his", "him"), "female": ("she", "her", "her"), "fluid": ("they", "their", "them")}
 
 
+def get_incompatible_traits(trait_1_list, trait_2_list, incompatible_map):
+    trait_1 = random.choice(trait_1_list)
+    if trait_1 in incompatible_map.keys():
+        trait_2_compatible_list = [trait for trait in trait_2_list if trait not in incompatible_map[trait_1]]
+    else:
+        trait_2_compatible_list = trait_2_list
+    trait_2 = random.choice(trait_2_compatible_list)
+    return trait_1, trait_2
+
+
+def pronounify_sentence(sentence, ch_pronouns_map):
+    sentence = sentence.replace("$pr0", ch_pronouns_map[0])
+    sentence = sentence.replace("$pr1", ch_pronouns_map[1])
+    sentence = sentence.replace("$pr2", ch_pronouns_map[2])
+    return sentence
+
+
 def generate_story_sentence(seed=0):
     random.seed(seed)
+
+    # Protagonist description
     pro_sex = random.choice(ch_sex)
     pro_pronoun = pronouns_map[pro_sex]
-    pro_quality = random.choice(ch_quality)
-    if pro_quality in incompatible_traits_map.keys():
-        flaw_list = [flaw for flaw in ch_flaw if flaw not in incompatible_traits_map[pro_quality]]
-    else:
-        flaw_list = ch_flaw
-    pro_flaw = random.choice(flaw_list)
+    pro_quality, pro_flaw = get_incompatible_traits(ch_quality, ch_flaw, incompatible_traits_map)
     pro_profession = random.choice(ch_profession)
-    pro_goal = random.choice(ch_goal)
-    if pro_goal in incompatible_reasons_map.keys():
-        reason_list = [reason for reason in ch_reason if reason not in incompatible_reasons_map[pro_goal]]
-    else:
-        reason_list = ch_reason
-    # print(reason_list)
-    pro_reason = random.choice(reason_list)
-    pro_reason = pro_reason.replace("$pr1", pro_pronoun[1])
+    pro_goal, pro_reason = get_incompatible_traits(ch_goal, ch_reason, incompatible_reasons_map)
+    pro_reason = pronounify_sentence(pro_reason, pro_pronoun)
+
+    # Antagonist description
+    ant_sex = random.choice(ch_sex)
+    ant_pronoun = pronouns_map[ant_sex]
     ant_flaw = random.choice([flaw for flaw in ch_flaw if flaw != pro_flaw])
     ant_profession = random.choice([profession for profession in ch_profession if profession != pro_profession])
+    ant_reason = random.choice([reason for reason in ch_reason if reason != pro_reason])
+    ant_reason = pronounify_sentence(ant_reason, ant_pronoun)
     story = (f"This is the story of a {pro_quality} but {pro_flaw} {pro_profession} "
              f"who wants to {pro_goal} to {pro_reason} "
-             f"but a {ant_flaw} {ant_profession} won't let {pro_pronoun[2]} do it.")
+             f"but a {ant_flaw} {ant_profession} won't let {pro_pronoun[2]} do it "
+             f"because {ant_pronoun[0]} wants to {ant_reason}.")
     return story
 
 
 if __name__ == '__main__':
-    for rn_seed in range(0, 5):
+    for rn_seed in range(0, 6):
         gen_story = generate_story_sentence(rn_seed)
         print(gen_story)
